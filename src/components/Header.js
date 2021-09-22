55d7af8e21431b63605943b82772021f431f3ff2
jsoonee <jsoonee12@gmail.com>
2021-09-22 20:39:51 +0900

    first work

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="sec header">
+            <div className="head">
+                <div className="flexbox">
+                    <div className="logo">
+                        SOONEESHOP
+                    </div>
+                    <div className="nav">
+                        <ul>
+                            <li>Contact</li>
+                            <li>Event</li>
+                            <li>Cart</li>
+                            <li>Login</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import belt from '../images/belt.png';
+
+const Main = () => {
+    return (
+        <main>
+            <div>
+                <img src={belt} alt="belt"></img>
+                <img src={belt} alt="belt"></img>
+                <img src={belt} alt="belt"></img>
+            </div>
+        </main>
+    );
+}
+
+export default Main;
