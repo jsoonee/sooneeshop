9a8e39d22ac052cbe07dcda23b0e604170cb059f
jsoonee <jsoonee12@gmail.com>
2021-10-19 15:49:01 +0900

    solve useeffect error

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,35 +2,34 @@ import React, { useState, useEffect } from 'react';
 
 const Filter = () => {
     const [active, setActive] = useState('');
-    useEffect(() => {
-        applyFilter();
-    }, [active]);
 
     const cats = ['top', 'bottom', 'outer', 'acc'];
     const cat = cats.map((el, index) => {
-
         return (
             <li key={index} onClick={() => handleOnClick(index)} className={active === index ? 'active' : 'unactive'}>{el}</li>
         );    
     })
-
+    
     const selPdt = document.querySelectorAll('.pdt');
     const selCat = document.querySelectorAll(`.${cats[active]}`);
 
-    const applyFilter = () => {
-        if (active === '') {
-            selPdt.forEach((li) => {
-                return (li.style.display = 'block');
-            });
-        } else {
-            selPdt.forEach((li) => {
-                li.style.display = 'none';
-            });
-            selCat.forEach((li) => {
-                li.style.display = 'block';
-            })
+    useEffect(() => {
+        const applyFilter = () => {
+            if (active === '') {
+                selPdt.forEach((li) => {
+                    return (li.style.display = 'block');
+                });
+            } else {
+                selPdt.forEach((li) => {
+                    li.style.display = 'none';
+                });
+                selCat.forEach((li) => {
+                    li.style.display = 'block';
+                })
+            }
         }
-    }
+        applyFilter();
+    }, [selPdt, selCat, active]);
 
     const handleOnClick = (index) => {
         if (index === active) {
